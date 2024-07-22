48281dee80fe52843a270b71cc58a62371717710
yeti-s <sel9371@gmail.com>
2024-07-22 14:24:48 +0000

    fix: split functions

--- quantize.py
+++ quantize.py
@@ -1,17 +1,50 @@
 import argparse
 import torch
+from dataset import TrainDataset
+from torch.utils.data import DataLoader
 
 from OverNet import Network
-from quantization.quant2d import quantize
+from quantization.quant2d import get_boundaries, quantize_dobi, distilate
+from quantization.eval import TestDataset
+
+scale, upscale = 4, 4
+
+def load_model(checkpoint):
+    state_dict = torch.load(checkpoint, map_location='cpu')['model_state_dict']
+    teacher, student = Network(), Network()
+    teacher.load_state_dict(state_dict)
+    student.load_state_dict(state_dict)
+    return teacher, student
+
+def quantize(model, calib_data, n_bits, device):
+    dobi_calib_dataset = TrainDataset(calib_data, 64, scale)
+    dobi_calib_loader = DataLoader(dobi_calib_dataset, 32, True)
+    boundaries = get_boundaries(model, dobi_calib_loader, scale, upscale, n_bits)
+    
+    model.cpu()
+    quantize_dobi(model, boundaries, '', n_bits)
+    model.to(device)
+
+def calibrate(teacher, student, calib_data, val_data):
+    calib_dataset = TrainDataset(calib_data, 64, scale)
+    calib_loader = DataLoader(calib_dataset, 16, True)
+    val_dataset = TestDataset(val_data, scale)
+    val_loader = DataLoader(val_dataset, 1, False)
+    distilate(teacher, student, calib_loader, val_loader, scale, upscale)
 
 def main(args):
-    model = Network()
+    calib_data, val_data = args.calib_data, args.val_data
     scale, upscale = args.scale, args.upscale
-    model.set_scale(scale, upscale)
-    checkpoint = torch.load(args.checkpoint, map_location='cpu')
-    model.load_state_dict(checkpoint['model_state_dict'])
+    n_bits, device = args.bits, args.device
     
-    quantize(model, args.calib_data, args.val_data, scale, upscale, args.device, n_bits=args.bits)
+    teacher, student = load_model(args.checkpoint)
+    teacher.set_scale(scale, upscale)
+    teacher.to(device)
+    student.set_scale(scale, upscale)
+    student.to(device)
+    
+    quantize(student, calib_data, n_bits, device)
+    calibrate(teacher, student, calib_data, val_data)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
